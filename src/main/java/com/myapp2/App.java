603e843f8fb5e6f658cd85c61ae242989bf4c9f1
pnowak86 <p_nowak86@poczta.fm>
2017-04-21 20:59:01 +0200

    commit2

--- src/main/java/com/myapp2/App.java
+++ src/main/java/com/myapp2/App.java
@@ -1,13 +1,27 @@
-package com.myapp2;
+package com.myapp;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello Wordlna!");
+        String a = "88.8";
+        String b = "235";
+        int c = 13;
+
+
+    }
+
+    public static void calculatePosition()
+    {
+        x(t) = 0,5 x at2 + vit + xi
+
+
+
     }
 }
